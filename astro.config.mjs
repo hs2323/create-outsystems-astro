3f610711aa750c40b335a1b79e0108492b7de38b
hs2323 <hs2323@gmail.com>
2025-10-20 21:12:43 -0400

    Updating build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,22 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes('node_modules') || id.includes('src')) {
+              return 'app.js';
+            }
+            return 'app.js';
+          },
+          entryFileNames: `[name].js`,
+          chunkFileNames: `[name].js`,
+          assetFileNames: `[name].[ext]`,
+        },
+      },
+    },
+  },
 });
