251a5f70f163fe54faa8caf9257c57be53b1406b
hs2323 <hs2323@gmail.com>
2026-01-09 08:14:42 -0500

    Adding tests for generator

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import prompts from "prompts";
-import { execSync } from "child_process";
+import yargs from "yargs";
+import { hideBin } from 'yargs/helpers';
+import { execSync } from "node:child_process";
+
+prompts.override(
+  yargs(hideBin(process.argv)).parse()
+);
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
