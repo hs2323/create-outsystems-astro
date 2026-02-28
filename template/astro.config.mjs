4ce0adf617f9e1adafb0aef0283a6ba325aff9cc
hs2323 <hs2323@gmail.com>
2026-02-28 15:03:31 -0500

    Adding Svelte counter example

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -5,7 +5,6 @@ import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
 import { defineConfig } from "astro/config";
 
-
 // https://astro.build/config
 export default defineConfig({
   build: {
