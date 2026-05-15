dee04c0213c6b8b673dc59d65efe083cb627cfdc
hs2323 <hs2323@gmail.com>
2026-05-15 07:35:49 -0400

    Set astro config assets path

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -11,6 +11,7 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   build: {
     inlineStylesheets: "always",
+    assets: 'assets', 
   },
   integrations: [
     angular({
