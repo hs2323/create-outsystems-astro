d0a7c95a923e27caf59901afe96bff12b2a7bfcd
hs2323 <hs2323@gmail.com>
2026-09-07 22:37:26 -0400

    Fixing Vanilla demo e2e

--- template/test/e2e/vanilla/vanilla-demo.spec.ts
+++ template/test/e2e/vanilla/vanilla-demo.spec.ts
@@ -6,7 +6,7 @@ test.beforeEach(async ({ page }) => {
 
 test.describe("Has values", () => {
   test("Should have header", async ({ page }) => {
-    await expect(page.getByText("VanillaJS Demo Component")).toBeVisible();
+    await expect(page.getByText("Vanilla JS Demo Component")).toBeVisible();
   });
 });
 
