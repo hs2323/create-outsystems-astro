44abe7e2f7df40e27f4a68a7d14a3cf474884a7e
hs2323 <hs2323@gmail.com>
2026-09-08 07:28:48 -0400

    Adding Qwik dependency

--- template/astro.config.mjs
+++ template/astro.config.mjs
@@ -5,6 +5,7 @@ import react from "@astrojs/react";
 import solid from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 import vue from "@astrojs/vue";
+import qwik from "@qwik.dev/astro";
 import { defineConfig } from "astro/config";
 import twig from "islands-integrations/twig";
 import vanilla from "islands-integrations/vanilla";
@@ -45,6 +46,9 @@ export default defineConfig({
     }),
     vue({
       include: ["src/framework/vue/*"],
+    }),
+    qwik({
+       include: ["src/framework/qwik/*"],
     }),
   ],
   server: {
