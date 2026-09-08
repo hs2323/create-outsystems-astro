2aedbcf17ce1a849eec5920795d6f2fbd17d5fd4
hs2323 <hs2323@gmail.com>
2026-09-07 22:24:23 -0400

    Updating demos for Svelte Runes

--- template/src/framework/svelte/Store.svelte
+++ template/src/framework/svelte/Store.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
+  import { useStore } from '@nanostores/svelte-runes';
+
   import SvelteLogo from "../../images/svelte.png?url";
   import { framework } from "../../stores/framework";
+
+  const currentFramework = useStore(framework);
 
   const setFramework = () => {
     framework.set("Svelte");
@@ -13,10 +17,10 @@
     <img alt="Svelte logo" src={SvelteLogo} height="150" />
     <div>
       <strong>Value:</strong>
-      <div id="nanostore">{$framework}</div>
+      <div id="nanostore">{currentFramework.current}</div>
     </div>
     <div>
-      <button class="card-btn" on:click={setFramework}>Select Svelte</button>
+      <button class="card-btn" onclick={setFramework}>Select Svelte</button>
     </div>
   </div>
 </div>
